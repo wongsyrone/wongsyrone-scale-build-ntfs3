650aac2703a64a77cc9f6b4938dd17531c60ae42
Syrone Wong <wong.syrone@gmail.com>
2023-04-16 15:25:41 +0800

    self build
    CI: split pkg build and update w/ iso
    CI: allow to trigger manually
    CI: make non-experimental by default
    
    ntfs3 patches from paragon group (unupstream)
    
    install pkgs
    
    single-branch
    
    add artifacts
    
    ignore error for make all step to get log artifacts
    
    china timezone
    
    linux: merge linux 6.18.16
    
    remove grub2 single job
    
     * this is leftover of previous bash-based build script
    
    add iso and update chksum files
    
    use latest pip packages
    
     * pin jsonschema due to schema exception
    
    CI: use git from Ubuntu PPA
    
    git: depth 1
    
    prepend sudo to cmdline
    
    show info after build
    
    CI: 9G root; 10G swap + 7G mem = 17G
    CI: temp 8G for artifacts upload
    
    CI: reclaim disk space
    
    https://github.com/truenas/zfs/commit/165d1157d69b790cc0ad0249663e95b60404194c
    
    CI: do not run in the background
    
    CI: setup-python github action uses this dir
    
    CI: only use setup-python for scale-build
    
    do NOT set PYTHON env
    
    CI: add comments and echoes
    
    disable packages with '-dbg' by removing them from manifest
    
    scst: back to official after cleanup dbg pkgs
    
    copy manifest from update dir
    
    use python 3.12
    
    do NOT show kernel config changes
    
    CI: exclude ccache dir
    
    CI: use sudo for commands on GH
    
    CI: set permission for tmp
    
    CI: cache rootfs and iso chroot
    
    output flag on whether rootfs and iso chroot cache is usable to /tmp
    
    CI: always upload cache when success build
    
    CI: use unversioned cache
    CI: always store pkg cache to avoid mass rebuild
    
    delete old one if we can upload new(update existing cache is not
    possible)
    
    see: github repo actions/toolkit issues/505
    
    all pkg DEB_BUILD_OPTIONS default to nocheck
    
    add check to copying finished package
    
    zfs prefer to local APT repo
    
    prefer to dist-security over dist
    but lower than local APT registry
    - disabled due to dist is newer than dist-security
    
    rebuild local APT repo if removed previous packages
    
    escape double quotes in commands to fix DEB_BUILD_OPTIONS handling
    
    Disable automatic building dbgsym packages
    ALL DEB_BUILD_OPTIONS: https://manpages.debian.org/testing/debhelper/debhelper.7.en.html#Supported_flags_in_DEB_BUILD_OPTIONS
    
    truenas_webui: disable node_modules.tgz output when creating tarfile
    package: add pkg name to ease reading
    
    truenas-installer: add back root user option
    - revert back to official repo when https://github.com/truenas/middleware/blob/master/src/freenas/usr/local/bin/truenas-set-authentication-method.py
      removes 'root' user handling
    
    truenas_verify: use my fork to bypass calculation
    
    switch to my spdk to remove debug kernel stuff

--- scale_build/packages/clean.py
+++ scale_build/packages/clean.py
@@ -9,16 +9,17 @@ class BuildCleanMixin:
     def clean_previous_packages(self):
         if not os.path.exists(self.pkglist_hash_file_path):
             # Nothing to do
-            return
+            return False
 
         with open(self.pkglist_hash_file_path, 'r') as f:
             to_remove = [p for p in map(str.strip, f.read().split()) if p]
 
         os.unlink(self.pkglist_hash_file_path)
         if not to_remove:
-            return
+            return False
 
-        self.logger.debug('Removing previously built packages for %s:', self.name)
         for name in to_remove:
+            self.logger.debug(f'Removing previously built packages for {self.name}: {name}')
             with contextlib.suppress(OSError, FileNotFoundError):
                 os.unlink(os.path.join(PKG_DIR, name))
+        return True
