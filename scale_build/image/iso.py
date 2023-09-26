a4b20d9fe2e6c8c0288a0c27909e408570c1b770
Syrone Wong <wong.syrone@gmail.com>
2023-09-26 10:04:17 +0800

    output apt-cache policy

--- scale_build/image/iso.py
+++ scale_build/image/iso.py
@@ -37,6 +37,7 @@ def install_iso_packages_impl():
 
     # echo "/dev/disk/by-label/TRUENAS / iso9660 loop 0 0" > ${CHROOT_BASEDIR}/etc/fstab
     for package in get_manifest()['iso-packages']:
+        run_in_chroot(['apt-cache', 'policy', package], log=True)
         run_in_chroot(['apt', 'install', '-y', package])
 
     # We want to make sure that truenas-installer service is enabled
@@ -126,6 +127,10 @@ def make_iso_file():
         run(['mount', '--bind', CD_DIR, os.path.join(CHROOT_BASEDIR, CD_DIR)])
         run(['mount', '--bind', PKG_DIR, os.path.join(CHROOT_BASEDIR, 'packages')])
         run_in_chroot(['apt-get', 'update'], check=False)
+        run_in_chroot([
+            'apt-cache', 'policy', 'grub-common', 'grub2-common', 'grub-efi-amd64-bin',
+            'grub-pc-bin', 'mtools', 'xorriso'
+        ], log=True)
         run_in_chroot([
             'apt-get', 'install', '-y', 'grub-common', 'grub2-common', 'grub-efi-amd64-bin',
             'grub-pc-bin', 'mtools', 'xorriso'
